75bfc797b1689836af9e7ae0ff9a2743447064bf
Benjamin Moore <american.exceptionalist@gmail.com>
2017-10-02 00:18:16 -0400

    Don't reveal answers when requesting questions

--- server/QuestionProvider.js
+++ server/QuestionProvider.js
@@ -7,7 +7,7 @@ QuestionProvider.prototype.findById = function(id, callback) {
   this.db.collection('questions', function(error, questions) {
     if(error) callback(error);
     else {
-      questions.findOne({_id: Number(id)}, function(error, result) {
+      questions.findOne({_id: Number(id)}, {fields: {correctAnswers: 0}}, function(error, result) {
         if(error) callback(error);
         else callback(null, result);
       });
@@ -19,7 +19,7 @@ QuestionProvider.prototype.findMaxId = function(callback) {
   this.db.collection('questions', function(error, questions) {
     if(error) callback(error);
     else {
-      questions.findOne({}, {fields:{_id:1}, sort:{_id:-1}}, function(error, result) {
+      questions.findOne({}, {fields: {_id: 1}, sort: {_id: -1}}, function(error, result) {
         if(error) callback(error);
         else callback(null, result);
       });
@@ -31,7 +31,7 @@ QuestionProvider.prototype.findAll = function(callback) {
   this.db.collection('questions', function(error, questions) {
     if(error) callback(error);
     else {
-      questions.find().sort({_id:1}).toArray(function(error, result) {
+      questions.find().sort({_id: 1}).toArray(function(error, result) {
         if(error) callback(error);
         else callback(null, result);
       });
